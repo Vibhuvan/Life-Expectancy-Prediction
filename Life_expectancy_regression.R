26879bf026d2583ffdae77b9c1e22d68be4c0b92
Vibhuvan <108579024+Vibhuvan@users.noreply.github.com>
2022-11-09 23:11:29 +0530

    Update Life_expectancy_regression.R

--- Life_expectancy_regression.R
+++ Life_expectancy_regression.R
@@ -1,7 +1,7 @@
 library(regclass)
 library(lmtest)
 library(orcutt)
-library(car)
+
 #Read file
 df<-read.csv("Life_expectancy.csv")
 
@@ -12,7 +12,7 @@ for(i in 4:ncol(df)){
 
 #Consider all variables
 df
-fit1 = lm(log(Life.expectancy) ~ Adult.Mortality+infant.deaths+Alcohol+percentage.expenditure+Hepatitis.B+Measles+
+fit1 = lm(Life.expectancy ~ Adult.Mortality+infant.deaths+Alcohol+percentage.expenditure+Hepatitis.B+Measles+
             BMI+under.five.deaths+Polio+Total.expenditure+Diphtheria+HIV.AIDS+GDP+Population+thinness..1.19.years+thinness.5.9.years+Income.composition.of.resources+Schooling, data=df)
 
 #Apply backward step to get best subset of variables
@@ -24,10 +24,10 @@ VIF(fit1)
 shapiro.test(residuals(fit1))
 
 #Apply log to a few variables as a remedy for non-normal residuals and multi-collinearity
-fit4 = lm(formula = Life.expectancy ~ Adult.Mortality +  
-            Alcohol + percentage.expenditure + Hepatitis.B + Measles + 
-            BMI + under.five.deaths + Polio + Total.expenditure + Diphtheria + 
-            HIV.AIDS + GDP + Population + thinness..1.19.years + 
+fit4 = lm(formula = Life.expectancy ~ log(Adult.Mortality) +  
+            Alcohol + (percentage.expenditure) + log(Hepatitis.B) + Measles + 
+            BMI + under.five.deaths + Polio + log(Total.expenditure) + Diphtheria + 
+            HIV.AIDS + log(GDP) + log(Population) + thinness..1.19.years + 
             Income.composition.of.resources + Schooling, data = df)
 
 #Tests on the transformed variables
@@ -41,13 +41,8 @@ coch = cochrane.orcutt(fit4)
 #Apply gg-plots and other plot as well
 fit_final = lm(coch)
 gqtest(fit_final, alternative='two.sided')
-qqnorm(residuals(fit_final), pch=19)
-qqline(residuals(fit_final))
-summary(coch)
-shapiro.test(residuals(fit_final))
-
-par(mfrow = c(2,2))
-hist(df$Life.expectancy)
-hist(residuals(fit_final))
+par(mfrow=c(2,2))
 plot(fitted(fit_final), residuals(fit_final))
-plot(fitted(fit4), residuals(fit4))
+plot(fitted(fit1), residuals(fit1))
+hist(residuals(fit_final))
+summary(fit_final)
